5e13e22137af5e38160fbf3cba02e7033ae5e038
Kevin0710gif <installminecraft1@gmail.com>
2024-11-05 21:31:44 +0800

    Add files via upload

---
+++ git.bat
@@ -0,0 +1,7 @@
+powershell winget install --id Git.Git -e --source winget
+powershell git clone https://github.com/Kevin0710gif/insmin.git
+cd insmin
+cd bat
+powershell Set-ExecutionPolicy Unrestricted
+powershell start.ps1
+
