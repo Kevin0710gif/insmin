2347d2dccb76efa67eceb2cdbcd63434ade0c734
Kevin0710gif <installminecraft1@gmail.com>
2024-11-10 17:31:08 +0800

    Delete git.bat

--- git.bat
+++
@@ -1,9 +0,0 @@
-cd c:\windows\system32\
-powershell winget install --id Git.Git -e --source winget
-powershell git clone https://github.com/Kevin0710gif/insmin.git
-cd insmin
-cd bat
-powershell Set-ExecutionPolicy Unrestricted
-powershell c:\windows\system32\insmin\bat\start.ps1
-pause
-
